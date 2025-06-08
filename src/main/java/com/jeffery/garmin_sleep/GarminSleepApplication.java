3ca62e6670fafe490c543dc71a02b52169c5ba4a
Jeffery Liu <a4264428@gmail.com>
2025-06-08 22:21:44 +0800

    initial commit

---
+++ src/main/java/com/jeffery/garmin_sleep/GarminSleepApplication.java
@@ -0,0 +1,13 @@
+package com.jeffery.garmin_sleep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarminSleepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarminSleepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeffery/garmin_sleep/GarminSleepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffery.garmin_sleep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarminSleepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
